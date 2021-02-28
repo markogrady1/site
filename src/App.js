d37aac56a77f0794cc3aef458a47b13147602d01
Mark O Grady <mark.o'grady@broadbean.com>
2021-02-28 17:00:37 +0000

    check if post is causing errors

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
 import Header from './components/Header';
 import Home from './components/Home';
 
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Projects from './components/pages/Projects';
 import Blog from './components/pages/Blog';
 import Notes from './components/pages/Notes';
 // import Post from './components/Post';
-// import Bookmarks from './components/pages/Bookmarks';
+import Bookmarks from './components/pages/Bookmarks';
 
 import './App.css';
 
-// const routes = [
-//   { component: Home, path: '/', },
-//   { component: Projects, path: '/projects', },
-//   { component: Blog, path: '/blog', },
-//   { component: Post, path: '/post/:id', },
-//   { component: Notes, path: '/notes', },
-//   { component: Bookmarks, path: '/bookmarks', }
-// ];
+const routes = [
+  { component: Home, path: '/', },
+  { component: Projects, path: '/projects', },
+  { component: Blog, path: '/blog', },
+  // { component: Post, path: '/post/:id', },
+  { component: Notes, path: '/notes', },
+  { component: Bookmarks, path: '/bookmarks', }
+];
 
 function App() {
 
+
   return (
-    <div className="App">
-      {/* <HashRouter basename='/'></HashRouter> */}
-      {/* <Header /> */}
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-        </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-        </a>
-      {/* <HashRouter /> */}
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          {routes.map((route, index) => {
+            return <Route key={index} path={route.path} exact component={route.component} />
+          })}
+        </Switch>
+      </Router>
+    </>
   );
-  // return (
-  //   <div>
-  //     <HashRouter basename='/'>
-  //       <Header />
-  //       {routes.map((route, index) => {
-  //         return <Route key={index} path={route.path} exact component={route.component} />
-  //       })}
-  //     </HashRouter>
-  //   </div>
-  // );
 }
 
 export default App;
