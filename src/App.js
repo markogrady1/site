78213247b86b3f920008722fd4da80c424846006
Mark O Grady <mark.o'grady@broadbean.com>
2021-02-28 18:43:59 +0000

    disable app until issue fixed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import Header from './components/Header';
 import Home from './components/Home';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Projects from './components/pages/Projects';
-import Blog from './components/pages/Blog';
-import Notes from './components/pages/Notes';
+// import Projects from './components/pages/Projects';
+// import Blog from './components/pages/Blog';
+// import Notes from './components/pages/Notes';
 // import Post from './components/Post';
-import Bookmarks from './components/pages/Bookmarks';
+// import Bookmarks from './components/pages/Bookmarks';
 
 import './App.css';
 
 const routes = [
   { component: Home, path: '/', },
-  { component: Projects, path: '/projects', },
-  { component: Blog, path: '/blog', },
-  // { component: Post, path: '/post/:id', },
-  { component: Notes, path: '/notes', },
-  { component: Bookmarks, path: '/bookmarks', }
+  // { component: Projects, path: '/projects', },
+  // { component: Blog, path: '/blog', },
+  // // { component: Post, path: '/post/:id', },
+  // { component: Notes, path: '/notes', },
+  // { component: Bookmarks, path: '/bookmarks', }
 ];
 
 function App() {
