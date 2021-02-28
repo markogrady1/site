3897440b256a09b06dc423c1d2ffcf512318ab50
Mark O Grady <mark.o'grady@broadbean.com>
2021-02-28 16:46:13 +0000

    more debugging

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import { HashRouter, Route } from "react-router-dom";
 import Projects from './components/pages/Projects';
 import Blog from './components/pages/Blog';
-// import Notes from './components/pages/Notes';
+import Notes from './components/pages/Notes';
 // import Post from './components/Post';
 // import Bookmarks from './components/pages/Bookmarks';
 
